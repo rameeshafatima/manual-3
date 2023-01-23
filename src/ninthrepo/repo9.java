fa930010e1ad6114da2ca6cc8b28c0e9b38c6045
Md Mujahid ul haq <Md Mujahid ul haq@DESKTOP-ODT7B24>
2023-01-22 23:30:33 -0600

    Testing poll scm

--- src/ninthrepo/repo9.java
+++ src/ninthrepo/repo9.java
@@ -5,6 +5,7 @@ public class repo9 {
 	public static void main(String[] args) {
 		System.out.println("My ninth repository");
 		System.out.println(90+68);
+		System.out.println("testing poll scm");
 
 	}
 
