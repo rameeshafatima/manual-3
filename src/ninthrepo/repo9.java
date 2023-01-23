1ba50468f41c3505c7666f2181c5d5e20ded24e0
Md Mujahid ul haq <Md Mujahid ul haq@DESKTOP-ODT7B24>
2023-01-23 10:01:07 -0600

    Testing

--- src/ninthrepo/repo9.java
+++ src/ninthrepo/repo9.java
@@ -6,6 +6,7 @@ public class repo9 {
 		System.out.println("My ninth repository");
 		System.out.println(90+68);
 		System.out.println("testing poll scm");
+		System.out.println("language of computers");
 
 	}
 
